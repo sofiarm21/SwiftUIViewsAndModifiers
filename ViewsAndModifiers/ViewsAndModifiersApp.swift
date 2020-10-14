960bb4472f12ced3257333ed32f8f23c435deef4
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-10-14 00:09:38 -0400

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Sofia Rodriguez Morales on 10/14/20.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
